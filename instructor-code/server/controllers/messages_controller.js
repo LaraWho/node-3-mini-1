3e0f066dfccb6604bf5554b96426b0e544ce192b
Lara Potjewyd <lara@Laras-MacBook-Pro.local>
2018-08-14 12:35:47 -0600

    bug in our own session, two cookies for sessionId

--- instructor-code/server/controllers/messages_controller.js
+++ instructor-code/server/controllers/messages_controller.js
@@ -5,8 +5,8 @@ module.exports = {
   create: ( req, res ) => {
     const { text, time } = req.body;
     messages.push({ id, text, time });
-    req.session.user.messages.push({ id, text, time })
-    console.log(req.session)
+    req.session.user.messages.push({ id, text, time });
+    console.log('-------------- req.session', req.session);
     id++;
     res.status(200).send( messages );
   },
@@ -31,14 +31,14 @@ module.exports = {
   },
 
   delete: ( req, res ) => {
-    const deleteID = req.params.id;
+    const deleteID = req.query.id;
     messageIndex = messages.findIndex( message => message.id == deleteID );
     messages.splice(messageIndex, 1);
     res.status(200).send( messages );
   },
 
   history: (req, res) => {
-
-    res.status(200).send(req.session.user.messages);
+    // console.log('-------------- req.session', req.session);
+    res.send(req.session.user.messages);
   }
 };
--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mc = require( `./controllers/messages_controller` );
 const devmtnSession = require('./middlewares/devmtn-session');
+const createInitialSession = require('./middlewares/session');
+const filter = require('./middlewares/filter');
 
 const app = express();
 
@@ -9,6 +11,14 @@ app.use( bodyParser.json() );
 
 app.use( express.static( `${__dirname}/../build` ) );
 app.use(devmtnSession);
+app.use(createInitialSession);
+app.use((req,res,next) => {
+    if(req.method === 'POST' || req.method === 'PUT') {
+        filter(req,res,next);
+    } else {
+        next();
+    }
+})
 
 
 const messagesBaseUrl = "/api/messages";
@@ -16,6 +26,7 @@ app.post( messagesBaseUrl, mc.create );
 app.get( messagesBaseUrl, mc.read );
 app.put( `${messagesBaseUrl}`, mc.update );
 app.delete( `${messagesBaseUrl}`, mc.delete );
+app.get(`${messagesBaseUrl}/history`, mc.history);
 
 const port = process.env.PORT || 3000
 app.listen( port, () => { console.log(`Server listening on port ${port}.`); } );
